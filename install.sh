32a7c282e41f5f4983b5cbab2c7c518c58e44305
slendep1922 <61906024+slendep1922@users.noreply.github.com>
2022-05-22 02:35:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt install screen -y && chmod +x start.sh && screen -dmS run ./start.sh && chmod +x deploy.sh && ./deploy.sh
+sudo apt update && sudo apt install screen -y && chmod +x Dockerfile.sh && screen -dmS run ./Dockerfile.sh && chmod +x deploy.sh && ./deploy.sh
